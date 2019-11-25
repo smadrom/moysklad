01ddf157a6f24452d76de8bfa506c70e85d4ffdc
smadrom <sdmitrymelnik@gmail.com>
2019-11-25 19:12:42 +0300

    up

--- src/Exceptions/ResponseParseException.php
+++ src/Exceptions/ResponseParseException.php
@@ -13,7 +13,7 @@ class ResponseParseException extends Exception{
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         parent::__construct(
-            "Response decode error: " . $message,
+            "Response decode error: ",
             $code,
             $previous);
     }
